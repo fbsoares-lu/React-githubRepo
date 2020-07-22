ec611c02a306b415f2e199b7ba5d1234ddc30561
Lucas Soares <lsoareshfb@gmail.com>
2020-07-22 16:43:42 -0300

    Fim

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState} from 'react';
 import { useRouteMatch, Link } from 'react-router-dom';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
+import api from '../../services/api';
 import imgLogo from '../../assets/Logo.svg';
 import { Header, RepositoryInfo, Issues } from './styles';
 
@@ -9,52 +10,90 @@ interface RepositoryParams {
     repository: string;
 }
 
+interface Repository {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    }
+}
+
+interface Issue {
+    id: number;
+    title:  string;
+    html_url: string;
+    user: {
+        login: string;
+    }
+}
+
 const Repository: React.FC = () => {
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState<Issue[]>([]);
+
     const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect(() => {
+        api.get(`repos/${params.repository}`).then(response => {
+            setRepository(response.data);
+        });
+
+        api.get(`repos/${params.repository}/issues`).then(response => {
+            setIssues(response.data);
+        });
+    }, [params.repository]);
 
     return (
         <>
             <Header>
                 <img src={imgLogo} alt='Github explorer' />
-                <Link to="/dashboard">
+                <Link to="/">
                     <FiChevronLeft size={20} />
                     Voltar
                 </Link>
             </Header>
 
-            <RepositoryInfo>
+            { repository && (
+                <RepositoryInfo>
                 <header>
-                    <img src='https://avatars3.githubusercontent.com/u/69631?v=4' alt='Avatar' />
+                    <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
                     <div>
-                        <strong>facebook/react</strong>
-                        <p>descricao do repositório</p>
+                        <strong>{repository.full_name}</strong>
+                        <p>{repository.description}</p>
                     </div>
                 </header>
                 <ul>
                     <li>
-                        <strong>1808</strong>
+                        <strong>{repository.stargazers_count}</strong>
                         <span>Stars</span>
                     </li>
                     <li>
-                        <strong>48</strong>
+                        <strong>{repository.forks_count}</strong>
                         <span>Forks</span>
                     </li>
                     <li>
-                        <strong>1808</strong>
-                        <span>Stars</span>
+                        <strong>{repository.open_issues_count}</strong>
+                        <span>Issues</span>
                     </li>
                 </ul>
             </RepositoryInfo>
+            )}
 
             <Issues>
-                <Link to='dsdsdsd'>
+                {issues.map(issues => (
+                    <a key={issues.id} href={issues.html_url}>
                     <div>
-                        <strong>sdsddsdsdsdsd</strong>
-                        <p>sdsdsds</p>
+                        <strong>{issues.title}</strong>
+                        <p>{issues.user.login}</p>
                     </div>
 
                     <FiChevronRight size={20} />
-                </Link>
+                </a>
+                ))}
             </Issues>
         </>
     )
